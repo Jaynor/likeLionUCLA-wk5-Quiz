42de7ee889d762c16cb4feffdf6cb31af34c0e50
Junyoung Kim <jkim7307@gmail.com>
2017-02-07 19:53:07 -0800

    Junyoung Kim

--- test.rb
+++ test.rb
@@ -1,5 +1,26 @@
 # Q1: Create a fizzBuzz method takes only one argument : def fizzBuzz (number)
 
+1. declare function take an integer
+2. if modulo(15) of int is 0
+3. print fizzbuzz
+4. elseif modulo(5) of int is 0
+5. print buzz  
+6. else modulo(3) of int is 0
+7. print buzz
+8. end
+
+
+
 # Q2: Create a method that prints out 'Christmas Tree' : def christmasTree (number)
 
+1. declare function take an integer n
+2. creat for loop condition from 0 to n 
+3. print (n-i'sp' + i of #) + (n+i'sp' + i-1 of #)
+end
+
 # Q3: Create a method that prints out total sum of numbers : def totalSum (string)
+1. declear function take string
+2. separate the input of string to integer number
+3. create for loop condition from 0 to the integer number
+4. add the integer number to sum
+5. print sum
